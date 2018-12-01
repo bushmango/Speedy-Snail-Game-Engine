08363f4ef314d6b3364851e413e8a66288ba5e39
nicross <nicross@users.noreply.github.com>
2018-12-01 10:40:56 -0600

    Add starfield essentials

---
+++ src/ludumDare43/actors/starfield.ts
@@ -0,0 +1,10 @@
+import { _ } from 'engine/importsEngine'
+import { getContext } from '../GameContext'
+import * as log from '../../engine/log'
+
+import * as spriteUtil from '../../engine/anim/spriteUtil'
+import * as anim from '../../engine/anim/anim'
+
+export function update(elapsedTimeSec, velocity) {
+  // TODO
+}
